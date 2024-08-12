5ff5468fc14ffd18c53ea629c1133d0c87a36fba
Chima <uc388002@dal.ca>
2024-08-12 01:22:47 -0400

    cicd test

--- server/application.py
+++ server/application.py
@@ -4,8 +4,8 @@ from flask_app import create_app
 application = create_app()
 
 if __name__ == "__main__":
-    application.run(host='0.0.0.0', port=8080, debug=True)
+    application.run(host='0.0.0.0', port=8080, debug=False)
 
 
 #references -> https://www.youtube.com/watch?v=7LNl2JlZKHA
-#access key updatee
+#access key updatee 
