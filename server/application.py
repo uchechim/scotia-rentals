2e58d2624e02154f62a3940191f21849e38225ae
Chima <uc388002@dal.ca>
2024-08-12 02:45:30 -0400

    cicd test

--- server/application.py
+++ server/application.py
@@ -4,7 +4,7 @@ from flask_app import create_app
 application = create_app()
 
 if __name__ == "__main__":
-    application.run(host='0.0.0.0', port=8080, debug=False)
+    application.run(host='0.0.0.0', port=80800, debug=False)
 
 
 #references -> https://www.youtube.com/watch?v=7LNl2JlZKHA
