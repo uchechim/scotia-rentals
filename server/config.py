66f2149a7d8f4c29e0531759a61b3a3eaa3317a4
Chima <uc388002@dal.ca>
2024-08-12 04:01:57 -0400

    cicd test

--- server/config.py
+++ server/config.py
@@ -2,7 +2,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 import os
-print(os.getenv('DB_HOST'))
 
 # Config class to access all environmental variables (abstracted from public) for this project
 class Config:
